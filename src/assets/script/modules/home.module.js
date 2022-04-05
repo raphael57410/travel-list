31e6932a3a9425545afbe36cc15682e26fc718b3
Raphael <demmerleraphael@hotmail.com>
2022-04-05 16:06:34 +0200

    ✨ affichage de la température d'une ville sans pouvoir la changer

--- src/assets/script/modules/home.module.js
+++ src/assets/script/modules/home.module.js
@@ -1,19 +1,47 @@
-import { headerFetch } from "../../utils/headerFetch";
+import { headerFetch } from "../../utils/headerFetch.js";
 
 export class Home {
+    _rootElement = document.querySelector('#root');
+    _cityName = 'paris';
+
     constructor() {
-        console.log('construct');
-        this.fetchWeather()
+        this.fetchWeather(this.cityName)
 
     }
 
-    fetchWeather() {
-        fetch('http://api.openweathermap.org/geo/1.0/direct?q=paris&limit=5&appid=1e94a8ed18cb346f1de036347bf337ec', headerFetch('GET'))
+    get cityName() {
+        return this._cityName;
+    }
+    set cityName(value) {
+        this._cityName = value;
+    }
+
+    fetchWeather(cityName) {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=1e94a8ed18cb346f1de036347bf337ec`, headerFetch('GET'))
             .then(res => {
                 if (res.ok) return res.json();
 
                 return Promise.reject(res);
             })
-            .then(response => console.log(response))
+            .then((response) => {
+                const lon = response[0].lon;
+                const lat = response[0].lat;
+                this.weatherApi(lon, lat);
+            })
+    }
+
+    weatherApi(lon, lat) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=1e94a8ed18cb346f1de036347bf337ec`, headerFetch('GET'))
+            .then(res => {
+                if (res.ok) return res.json();
+
+                return Promise.reject(res);
+            })
+            .then(response => {
+                const divElement = document.createElement('div');
+                divElement.innerText = 'Temperature de Paris: ' + response.main.temp + ' °C';
+
+                this._rootElement.appendChild(divElement);
+            })
     }
 }
